f0616b985df4e0687883929bbe157f56371b0083
nauam <nauamvictor@outlook.com>
2021-04-21 11:14:20 -0300

    Caracteristicas da linguagem 2

---
+++ 01-DevBasicJava/src/caracteristica2/ifs/IF.java
@@ -0,0 +1,22 @@
+package caracteristica2.ifs;
+
+public class IF {
+
+  public static void main(String[] args) {
+
+    final var condicao = true;
+
+    if (condicao) {
+      System.out.println("A condição é verdadeira");
+    } else {
+      System.out.println("A condição é falsa");
+    }
+
+    if (condicao)
+      System.out.println("Uma única linha...");
+
+    final var ternario = condicao ? "é verdadeira" : "é falsa";
+
+    System.out.println(ternario);
+  }
+}
