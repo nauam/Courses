5e78a9bf94998ca201740aba00ce5016aded6ac6
nauam <nauamvictor@outlook.com>
2021-09-22 15:25:25 -0300

    up

--- Rocketseat/youtube-masterclass-node/logger.js
+++ Rocketseat/youtube-masterclass-node/logger.js
@@ -5,13 +5,13 @@ const path = require('path')
 const emitter = new EventEmitter()
 
 emitter.on('log', (message) => {
-  fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
-    if (err) throw err
-  })
+    fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
+        if (err) throw err
+    })
 })
 
 function log(message) {
-  emitter.emit('log', message)
+    emitter.emit('log', message)
 }
 
 module.exports = log
