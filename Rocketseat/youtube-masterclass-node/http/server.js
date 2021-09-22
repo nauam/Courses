b43f61d29fb09100f9153c2dbf8dab62a3d79643
nauam <nauamvictor@outlook.com>
2021-09-22 15:02:29 -0300

    add api

--- Rocketseat/youtube-masterclass-node/http/server.js
+++ Rocketseat/youtube-masterclass-node/http/server.js
@@ -3,20 +3,20 @@ const fs = require('fs')
 const path = require('path')
 
 http.createServer((req, res) => {
-  const file = req.url === '/' ? 'index.html' : req.url
-  const filePath = path.join(__dirname, 'public', file)
-  const extname = path.extname(filePath)
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
 
-  const allowedFileTypes = ['.html', '.css', '.js']
-  const allowed = allowedFileTypes.find(item => item == extname)
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
 
-  if (!allowed) return
+    if (!allowed) return
 
-  fs.readFile(
-    filePath,
-    (err, content) => {
-      if (err) throw err
-      res.end(content)
-    }
-  )
-}).listen(5000, () => console.log('Server is running'))
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if (err) throw err
+            res.end(content)
+        }
+    )
+}).listen(5660, () => console.log('Server is running'))
