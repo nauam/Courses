1f7ce0f2226bee1e773bc52c3236a51914573998
nauam <nauamvictor@outlook.com>
2021-10-20 10:19:20 -0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@
 npm install
 npm run docs:build
 
-docker build -t nauam/docs .
+docker build -t qwsoftware/docs .
 docker login -u "nauam" -p "TDT7q76Tta2Ea" docker.io
-docker tag nauam/docs:latest nauam/docs:latest
-docker push nauam/docs:latest
+docker tag qwsoftware/docs:latest qwsoftware/docs:$1
+docker push qwsoftware/docs:$1
