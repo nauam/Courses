4285d2a729d7fabec8cfc40d4c153744a73286ee
nauam <nauamvictor@outlook.com>
2021-10-25 16:21:07 -0300

    Docker Compose

--- jenkins.sh
+++ jenkins.sh
@@ -6,5 +6,5 @@ cp -r docs/assets/ docs/.vuepress/dist/
 
 docker build -t qwsoftware/docs .
 docker login -u "idvlab" -p "VL84xFUAu5h2N" docker.io
-docker tag qwsoftware/docs:latest qwsoftware/docs:$1
+docker tag qwsoftware/docs qwsoftware/docs:$1
 docker push qwsoftware/docs:$1
